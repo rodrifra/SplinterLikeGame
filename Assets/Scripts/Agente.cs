542597ba692c23a79f4b152354868abb6d3e9c79
rodrifra <basurarusab@gmail.com>
2020-10-27 15:53:03 +0100

    Inicio con un par de vigilantes, creado Animation path y moviendose los guardias hacia los puntos objetivo

---
+++ Assets/Scripts/Agente.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Agente : MonoBehaviour
+{
+
+    public Transform[] objetivo;
+    NavMeshAgent nma;
+    int indiceObjetivos=0;
+
+    void Awake()
+    {
+        nma=GetComponent<NavMeshAgent>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        nma.SetDestination(objetivo[indiceObjetivos].position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nma.hasPath)
+        {
+            if (nma.remainingDistance < 1)
+            {
+              indiceObjetivos++;
+              indiceObjetivos = indiceObjetivos == objetivo.Length ? 0 : indiceObjetivos;
+              nma.SetDestination(objetivo[indiceObjetivos].position);
+            }
+        }
+    }
+}
